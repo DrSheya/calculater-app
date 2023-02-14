524315f6540246647d4f80f2aff220fa3ceeabb4
Sheya <115377434+DrSheya@users.noreply.github.com>
2023-02-14 18:58:19 +0100

    Added some new desinges
    Added some new styles
    And adden an addEventListener

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+// Get the input field and all the number buttons
+const inputField = document.getElementById('calculation');
+const numberButtons = document.querySelectorAll('.numpad-button');
+
+// Attach a click event listener to each button
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Get the value of the clicked button
+    const buttonValue = button.textContent;
+
+    // Append the button value to the input field
+    inputField.value += buttonValue;
+  });
+});
